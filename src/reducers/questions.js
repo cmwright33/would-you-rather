bbaaeabf99081321aacfc0d4a3d4884adf455463
Christopher Wright <christopherwright@Christophers-MacBook-Pro-3.local>
2019-07-02 16:48:44 -0400

    correct spelling errors for recieve, add redux packages, combine reducers, set up store in index.js

--- src/reducers/questions.js
+++ src/reducers/questions.js
@@ -1,9 +1,9 @@
-import	{ RECIEVE_QUESTIONS } from '../actions/questions'
+import	{ RECEIVE_QUESTIONS } from '../actions/questions'
 
 
 export default function	questions (state= {}, action) {
 	switch (action.type) {
-		case RECIEVE_QUESTIONS :
+		case RECEIVE_QUESTIONS :
 			return {
 				...state,
 				...action.questions
